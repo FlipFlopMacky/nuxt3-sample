41214d29ad6a2f977320b3a1e166361c9d6a1139
Fumiya Makino <flipflop.macky.github@gmail.com>
2022-05-26 00:40:53 +0900

    tailwindcss入れる修正中

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  // TODO: 動かないのでコメントアウト
+  // modules: ['@nuxtjs/tailwindcss'],
 })
